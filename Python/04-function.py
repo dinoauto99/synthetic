1151c53d0c7e41a25910cbb34606585e3cd6121f
LinHNK <khanhlin24291@gmail.com>
2024-03-11 20:36:58 +0700

    20240311 update functions

--- Python/04-function.py
+++ Python/04-function.py
@@ -13,3 +13,51 @@ def testFunc(arg):
 text = 'hello'
 testFunc(text)
 testFunc(123)
+
+
+def printInfo(name, age=20):
+    print('name:', name, '\tage:', age)
+    return
+
+
+printInfo('John', age=25)
+printInfo(name='Tom')
+
+
+def add(x, y):
+    z = x+y
+    print('x= {} \t y= {} \t x+y= {}'.format(x, y, z))
+    return
+
+
+add(1, 2)
+
+
+def adds(*args):
+    s = 0
+    for n in args:
+        s += n
+    return s
+
+
+print(adds(1, 2, 3, 4))
+
+
+def addr(**kwargs):
+    for k, v in kwargs.items():
+        print('{} {}'.format(k, v))
+    return
+
+
+addr(Name="John", Age=20, City="LA")
+
+mark = 10
+
+
+def variableScope():
+    # mark = 9
+    globals()['mark'] += 10
+    print('mark:', mark)
+
+
+variableScope()
