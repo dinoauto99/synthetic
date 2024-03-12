f9a0e0635f7ff50a2caad4f8e25dd0dfb7b0d834
LinHNK <khanhlin24291@gmail.com>
2024-03-12 22:05:14 +0700

    20240312 add theory

--- Python/04-function.py
+++ Python/04-function.py
@@ -61,3 +61,4 @@ def variableScope():
 
 
 variableScope()
+xcbcvb
---
+++ Python/05-exercises.py
@@ -0,0 +1,10 @@
+car = {
+    'brand': 'ford',
+    'model': 'mustag',
+    'year': 2010
+}
+car['color'] = 'red'
+car['year'] = 2020
+print(car.get('model'))
+print(car.get('year'))
+print(car.get('color'))
