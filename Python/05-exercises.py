d5102ab4e1ef43b33b2e7dbc09f44b145da336f4
LinHNK <khanhlin24291@gmail.com>
2024-03-14 21:49:20 +0700

    20240314 update theory

--- Python/05-exercises.py
+++ Python/05-exercises.py
@@ -1,3 +1,6 @@
+import datetime as dt
+from mymodule import rest as rr
+import mymodule as mm
 car = {
     'brand': 'ford',
     'model': 'mustag',
@@ -8,3 +11,48 @@ car['year'] = 2020
 print(car.get('model'))
 print(car.get('year'))
 print(car.get('color'))
+
+
+class mynumbers:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        if self.a < 10:
+            x = self.a
+            self.a += 1
+            return x
+        else:
+            raise StopIteration
+
+
+myclass = mynumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+    print(x)
+
+x = 200
+
+
+def testscope():
+    global x
+    x = 300
+    print(x)
+
+
+testscope()
+print(x)
+
+
+mm.greet()
+
+print(mm.mydict['year'])
+
+for x in dir(mm):
+    print(x)
+
+rr()
+
+print(dt.datetime.now())
---
+++ Python/mymodule.py
@@ -0,0 +1,12 @@
+def greet():
+    print('hello in greeting function')
+
+
+mydict = {
+    'brand': 'Ford',
+    'year': 2024
+}
+
+
+def rest():
+    print('only import this function')
