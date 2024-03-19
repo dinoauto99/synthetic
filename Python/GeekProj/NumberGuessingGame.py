2cd1bddaf25ef4f278f6127c3686a5632381529c
LinHNK <khanhlin24291@gmail.com>
2024-03-19 07:15:32 +0700

    20240319 update number guessing game

--- Python/GeekProj/NumberGuessingGame.py
+++ Python/GeekProj/NumberGuessingGame.py
@@ -12,14 +12,14 @@ min_guess = math.log2(max_range-min_range+1)
 count = 0
 while (count <= min_guess):
     user_guess = int(input("Guess number: "))
+    count += 1
     if user_guess < random_number:
         print("Try Again! You guessed too small")
     elif user_guess > random_number:
         print("Try Again! You guessed too high")
     else:
         break
-    count += 1
 if count > min_guess:
-    print("Better Luck Next Time!")
+    print("Better Luck Next Time!\nguessed {} times".format(count))
 else:
-    print("Congratulations! ")
+    print("Congratulations!\nguessed {} times".format(count))
